2ca683a0962cbf51201948ae6658715134107be6
Zero656329 <hwj656329@163.com>
2019-12-17 22:12:33 +0800

    初始化

---
+++ src/test/java/com/sunny/houlingyun/HoulingyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.houlingyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoulingyunApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
